dec8e8c4af713355df00acc4985a508e62d84b5e
CastrBG <78176365+CastrBG@users.noreply.github.com>
2023-09-28 21:06:31 +0300

    Uploading more problems solved

---
+++ Programming Basics/First steps in coding/03. Deposit Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int timePeriod = int.Parse(Console.ReadLine());
+            double rate = double.Parse(Console.ReadLine()) / 100;
+            double returnSum = deposit + timePeriod * ((deposit * rate) / 12);
+            Console.WriteLine(returnSum);
+        }
+    }
+}
---
+++ Programming Basics/First steps in coding/04. Vacation Books List/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace _04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bookPages = int.Parse(Console.ReadLine());
+            int readPagesPerHour = int.Parse(Console.ReadLine());
+            int daysToReadBook = int.Parse(Console.ReadLine());
+            int result = bookPages / readPagesPerHour / daysToReadBook;
+            Console.WriteLine(result);
+        }
+    }
+}
