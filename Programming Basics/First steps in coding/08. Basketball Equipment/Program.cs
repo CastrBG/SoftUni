9a1760d1611cd65d00dd333daccda9b6ca27f2ee
CastrBG <78176365+CastrBG@users.noreply.github.com>
2023-09-28 21:10:36 +0300

    Add files via upload

---
+++ Programming Basics/First steps in coding/08. Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int annualSubscription = int.Parse(Console.ReadLine());
+            double basketballShoes = annualSubscription * 0.6;
+            double basketballJersey = basketballShoes * 0.8;
+            double basketballBall = basketballJersey * 0.25;
+            double basketballAccessories = basketballBall * 0.2;
+            double result = annualSubscription + basketballShoes + basketballJersey + basketballBall + basketballAccessories;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Programming Basics/First steps in coding/09. Fish Tank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace _09._Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentDecoration = double.Parse(Console.ReadLine()) / 100;
+
+            double volume = (length * width * height) * 0.001;
+            double result = volume * (1 - percentDecoration);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
