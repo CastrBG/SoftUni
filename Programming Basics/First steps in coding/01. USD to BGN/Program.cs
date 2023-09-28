320f04d18a656198a702997b35cf03256d4ec6fe
CastrBG <78176365+CastrBG@users.noreply.github.com>
2023-09-28 21:04:07 +0300

    Uploading 01. USD to BGN

---
+++ Programming Basics/First steps in coding/01. USD to BGN/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+double inputDollars = double.Parse(Console.ReadLine());
+double fixedValueLev = 1.79549;
+Console.WriteLine(inputDollars * fixedValueLev);
