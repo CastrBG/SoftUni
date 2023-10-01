328538b0919fa4f05b25cd2016ae0dec2ac43c3e
CastrBG <78176365+CastrBG@users.noreply.github.com>
2023-10-01 22:14:34 +0300

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Print_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = int.Parse(Console.ReadLine());
+            int endingNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = startingNumber; i <= endingNumber; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/05. Login/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string usernameInput = Console.ReadLine();
+            string correctPassword = string.Join("", usernameInput.ToCharArray().Reverse()) ;
+            int incorrectCount = 0;
+           
+
+            while (true)
+            {
+                string passwordInput = Console.ReadLine();
+                if (passwordInput == correctPassword)
+                {
+                    Console.WriteLine($"User {usernameInput} logged in.");
+                    break;
+                }
+                else
+                {
+                    incorrectCount++;
+                    if (incorrectCount == 4)
+                    {
+                        Console.WriteLine($"User {usernameInput} blocked!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect password. Try again.");
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
