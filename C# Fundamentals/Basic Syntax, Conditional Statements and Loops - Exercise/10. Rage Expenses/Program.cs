b7065b3ca078785ddb0905cc1e71e4cb5389fff9
CastrBG <78176365+CastrBG@users.noreply.github.com>
2023-10-01 22:17:30 +0300

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/10. Rage Expenses/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int headsetTrashCount = 0;
+            int mouseTrashCount = 0;
+            int keyboardTrashCount = 0;
+            int displayTrashCount = 0;
+
+            for (int i = 1; i <= lostGamesCount; i++)
+            {
+                int currentHeadsetTrash = 0;
+                int currentMouseTrash = 0;
+                if (i % 2 == 0)
+                {
+                    headsetTrashCount++;
+                    currentHeadsetTrash++;
+                }
+                if (i % 3 == 0)
+                {
+                    mouseTrashCount++;
+                    currentMouseTrash++;
+                }
+                if (currentHeadsetTrash == 1 && currentMouseTrash == 1)
+                {
+                    keyboardTrashCount++;
+                }
+                if (keyboardTrashCount % 2 == 0 && currentMouseTrash == 1 && currentHeadsetTrash == 1)
+                {
+                    if (keyboardTrashCount == 0)
+                    {
+                        continue;
+                    }
+                    displayTrashCount++;
+                }
+            }
+
+            double rageExpenses = (headsetTrashCount * headsetPrice) + (mouseTrashCount * mousePrice)+ 
+                (keyboardTrashCount * keyboardPrice) + (displayTrashCount * displayPrice);
+
+            Console.WriteLine($"Rage expenses: {rageExpenses:f2} lv.");
+        }
+    }
+}
---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/11. Orders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orderAmount = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            for (int i = 0; i < orderAmount; i++)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double orderPrice = ((days * capsulesCount) * capsulePrice);
+                Console.WriteLine($"The price for the coffee is: ${orderPrice:f2}");
+                totalPrice+= orderPrice;
+            }
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
