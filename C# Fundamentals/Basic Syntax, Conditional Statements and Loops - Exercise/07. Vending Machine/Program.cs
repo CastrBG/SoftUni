7195ec6d585c8dd648bcd44ad0bb835b8acbe5b8
CastrBG <78176365+CastrBG@users.noreply.github.com>
2023-10-01 22:16:33 +0300

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/07. Vending Machine/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace _07._Vending_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double sum = 0;
+            while (command != "Start")
+            {
+                double input = double.Parse(command);
+                switch (input)
+                {
+                    case 0.1: sum += 0.1; break;
+
+                    case 0.2: sum += 0.2; break;
+
+                    case 0.5: sum += 0.5; break;
+
+                    case 1: sum += 1; break;
+
+                    case 2: sum += 2; break;
+
+                    default:
+                        Console.WriteLine($"Cannot accept {input}");
+                        command = Console.ReadLine();
+                        continue;
+                }
+                command = Console.ReadLine();
+            }
+
+            string product = Console.ReadLine();
+
+            while (product != "End")
+            {
+                bool canPurchase = true;
+                double productPrice = 0;
+                switch (product)
+                {
+                    case "Nuts": 
+                        double nutsPrice = 2.0;
+                        productPrice = nutsPrice;
+                        if (sum - nutsPrice < 0)
+                        {
+                            canPurchase= false;
+                        }
+                        break;
+                    case "Water":
+                        double waterPrice = 0.7;
+                        productPrice = waterPrice;
+                        if (sum - waterPrice < 0)
+                        {
+                            canPurchase = false;
+                        }
+                        break;
+                    case "Crisps":
+                        double crispsPrice = 1.5;
+                        productPrice = crispsPrice;
+                        if (sum - crispsPrice < 0)
+                        {
+                            canPurchase = false;
+                        }
+                        break;
+                    case "Soda":
+                        double sodaPrice = 0.8;
+                        productPrice = sodaPrice;
+                        if (sum - sodaPrice < 0)
+                        {
+                            canPurchase = false;
+                        }
+                        break;
+                    case "Coke":
+                        double cokePrice = 1.0;
+                        productPrice = cokePrice;
+                        if (sum - cokePrice < 0)
+                        {
+                            canPurchase = false;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        product= Console.ReadLine();
+                         continue;
+                }
+                if (canPurchase)
+                {
+                    Console.WriteLine($"Purchased {product.ToLower()}");
+                    sum -= productPrice;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry not enough money");
+                }
+                product = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Change: {sum:f2}");
+        }
+    }
+}
---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyAvailable = double.Parse(Console.ReadLine());
+            int studentsAmount = int.Parse(Console.ReadLine());
+            double lightsaberCost = double.Parse(Console.ReadLine());
+            double robeCost = double.Parse(Console.ReadLine());
+            double beltsCost = double.Parse(Console.ReadLine());
+
+
+            double lightsaberAmount = Math.Ceiling( studentsAmount * 1.1);
+            int beltsAmount = studentsAmount;
+            int n = studentsAmount;
+            while (true)
+            {
+                if (n - 6 >= 0)
+                {
+                    n -= 6;
+                    beltsAmount--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            double sum = (lightsaberCost * lightsaberAmount) + (robeCost * studentsAmount) + (beltsAmount * beltsCost);
+            if (moneyAvailable - sum >= 0)
+            {
+                Console.WriteLine($"The money is enough - it would cost {sum:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {Math.Abs(sum - moneyAvailable):f2}lv more.");
+            }
+        }
+    }
+}
