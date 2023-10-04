874a5aafaab3c5ceb12290479b2d8fee7e65f24b
CastrBG <78176365+CastrBG@users.noreply.github.com>
2023-10-04 22:52:51 +0300

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+            int coursesNeeded = 0;
+
+            while (numberOfPeople - elevatorCapacity > 0)
+            {
+                numberOfPeople -= elevatorCapacity;
+                coursesNeeded++;
+            }
+            if (numberOfPeople > 0)
+            {
+                numberOfPeople = 0;
+                coursesNeeded++;
+            }
+            
+            Console.WriteLine(coursesNeeded);
+        }
+    }
+}
